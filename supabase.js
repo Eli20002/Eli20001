fc4f0f3542b12c924c0019dcf87d71ae5c616d6a
C/xakin <cabdixakiinshiine0@gmail.com>
2026-08-13 10:15:28 +0300

    Update Supabase URL and anon key

--- supabase.js
+++ supabase.js
@@ -1,5 +1,8 @@
-const SUPABASE_URL = "GELI_SUPABASE_URL_HAADA";
-const SUPABASE_ANON_KEY = "GELI_SUPABASE_ANON_KEY_HAADA";
+const SUPABASE_URL =
+  "https://praguwyyshmhmmowofqv.supabase.co";
+
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByYWd1d3l5c2htaG1tb3dvZnF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY1NzA3NjYsImV4cCI6MjEwMjE0Njc2Nn0.oJjsk_93uBMLeT-8ydDeecJBpcKI4IwTZaVmDU0t-fk";
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
